4b32a2082741afa165fc48a400f20fb530eca4c0
Tony <caizongyou0319@gmail.com>
2025-05-07 19:12:19 +0800

    Add Krona One font and update page layout with navigation buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,47 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Krona_One } from "next/font/google";
+import { animate } from "animejs";
+
+const kronaOne = Krona_One({ subsets: ['latin'], weight: '400' });
 
 export default function Home() {
   return (
-    //icon
-    <div className="flex flex-justify-center items-center h-screen">
-      <div>
-        1
+    <div className="h-screen overflow-y-scroll snap-y snap-mandatory">
+      <div className="h-screen w-full flex flex-col snap-start snap-always">
+        <div className={`${kronaOne.className} text-[20px] w-full flex justify-end pt-[40px] pr-[60px]`}>
+          <div className="mr-[60px] mt-[30px]">
+            <button>MAIN</button>
+          </div>
+          <div className="mr-[60px] mt-[30px]">
+            <button>ABOUT</button>
+          </div>
+          <div className="mr-[60px] mt-[30px]">
+            <button>PROTFILIO</button>
+          </div>
+          <div className="mr-[60px] mt-[30px]">
+            <button>CONTACT</button>
+          </div>
+        </div>
+        <div className="flex-grow flex items-center">
+          <div className={`${kronaOne.className} text-[40px] font-bold ml-[40px]`}>
+            <div>
+              A CREATE DESIGNER
+            </div>
+            <div>
+              & DEVELOPER.
+            </div>
+          </div>
+        </div>
       </div>
-      <div>
-        2
+      <div className="h-screen w-full flex flex-col snap-start snap-always">
+        ABOUT
+      </div>
+      <div className="h-screen w-full flex flex-col snap-start snap-always">
+        PORTFOLIO
+      </div>
+      <div className="h-screen w-full flex flex-col snap-start snap-always">
+        CONTACT
       </div>
     </div>
   );
